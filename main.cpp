2172040cec4f290ba74c93a7278e04628cd5a1ad
Sharay <sharaygao@gmail.com>
2021-04-11 12:28:00 -0400

    Fixed errors, Bi-directional compiles and runs (there may be a logic error)

--- main.cpp
+++ main.cpp
@@ -13,15 +13,16 @@ class graph
     unordered_map<string, unordered_set<string>> findWords;
     int numOfWords;
 public:
-    graph(unordered_set<string> words,int size)
+    graph(unordered_set<string> words, int size)
     {
-        numOfWords=size;
+        numOfWords = size;
         for (auto iter = words.begin(); iter != words.end(); ++iter)
         {
             string cur = *iter;
             for (int i = 0; i < cur.length(); ++i)
             {
-                string insert = cur.substr(0, i) + "*" + cur.substr(1 + i, cur.length() - i - 1);
+                string insert = cur;
+                insert[i] = '*';
                 //inserts this particular combo into findWords
                 if (findWords.find(insert) == findWords.end())
                 {
@@ -55,31 +56,35 @@ public:
         return findWords.at(access);
     }
 };
-string BFS(const graph &graph, string begin, string end,int &visited)
+string BFS(const graph &graph, string begin, string end, int &visited)
 {
     vector<string> path;
-    if(begin==end)
+    if(begin == end)
     {
-        return begin+", "+end;
+        return begin + ", " + end;
     }
-    int visitedCount=1;
+
+    int visitedCount = 1;
     unordered_map<string,string> paths;
     queue<string> words;
+
     words.push(begin);
-    paths.insert({begin,""});
-    string current="";
-    while(current!=end&&!words.empty())
+    paths.insert({begin , ""});
+    string current = "";
+
+    while(current != end && !words.empty())
     {
-        current=words.front();
+        current = words.front();
         words.pop();
-        for(unsigned int i =0; i<current.length();++i)
+        for(unsigned int i = 0; i < current.length(); ++i)
         {
-            string temp=current;
-            temp[i]='*';
+            string temp = current;
+            temp[i] = '*';
             unordered_set<string> list = graph.getList(temp);
-            for(auto it=list.begin();it!=list.end();++it)
+
+            for(auto it = list.begin(); it != list.end(); ++it)
             {
-                if(paths.find(*it)==paths.end())
+                if(paths.find(*it) == paths.end())
                 {
                     paths.insert({*it, current});
                     words.push(*it);
@@ -89,121 +94,148 @@ string BFS(const graph &graph, string begin, string end,int &visited)
         }
     }
     string cheese = "";
-    if(current!=end)
-        cout<<"There is no word ladder from "<<begin<<" to "<<end<<endl;
+
+    if(current != end)
+        cout << "There is no word ladder from " << begin << " to " << end << endl;
     else
     {
-        while (current != begin) {
+        while (current != begin) 
+        {
             path.push_back(current);
             current = paths[current];
         }
+
         path.push_back(begin);
-        for (auto it = path.rbegin(); it != path.rend(); ++it) {
+
+        for (auto it = path.rbegin(); it != path.rend(); ++it) 
+        {
             if (it + 1 == path.rend())
                 cheese += *it;
             else
                 cheese += *it + ",";
         }
     }
+
     visited = visitedCount;
     return cheese;
 }
-string biBFS(const graph &graph, string begin, string end,int &visited)
+string biBFS(const graph &graph, string begin, string end, int &visited)
 {
-    string cheese="";
+    string cheese = "";
+
+    //this is so ugly
     unordered_map<string,string> forward_v;
     unordered_map<string,string> backward_v;
     queue<string> forward_q;
     queue<string> backward_q;
-    vector<string> f_path;
-    vector<string> b_path;
-    if(begin==end)
+    if(begin == end)
     {
-        return begin+", "+end;
+        return begin + ", " + end;
     }
     int visitedCount=1;
+
     forward_q.push(begin);
     backward_q.push(end);
-    string intersect="";
-    string front="";
-    string backward="";
-    while(!forward_q.empty()&&!backward_q.empty())
+
+    string intersect = "";
+    string front = "";
+    string back = "";
+
+    while(!forward_q.empty() && !backward_q.empty() && intersect == "")
     {
         front = forward_q.front();
         forward_q.pop();
-        backward = backward_q.front();
+        back = backward_q.front();
         backward_q.pop();
-        for(unsigned int i =0; i<front.length();++i)
+
+        for(unsigned int i =0; i < front.length(); ++i)
         {
-            string temp=front;
-            temp[i]='*';
+            string temp = front;
+            temp[i] = '*';
             unordered_set<string> list = graph.getList(temp);
-            for(auto it=list.begin();it!=list.end();++it)
+            for(auto it = list.begin(); it != list.end(); ++it)
             {
-                if(forward_v.find(*it)==forward_v.end())
+                if(forward_v.find(*it) == forward_v.end()) //if we haven't visited it before, we want to check it
                 {
+
+
                     forward_v.insert({*it, front});
-                    forward_q.push(*it);
+                    forward_q.push(*it); //line it up to be checked
                 }
                 ++visitedCount;
             }
         }
-        for(unsigned int i =0; i<backward.length();++i)
+
+        for(unsigned int i =0; i < back.length(); ++i)
         {
-            string temp=backward;
-            temp[i]='*';
+            string temp = back;
+            temp[i] = '*';
             unordered_set<string> list = graph.getList(temp);
-            for(auto it=list.begin();it!=list.end();++it)
+            for(auto it = list.begin(); it != list.end(); ++it)
             {
-                if(backward_v.find(*it)==backward_v.end())
+                if(backward_v.find(*it) == backward_v.end())
                 {
-                    backward_v.insert({*it, front});
+                    backward_v.insert({*it, back});
                     backward_q.push(*it);
                 }
                 ++visitedCount;
             }
         }
-        for(auto it = forward_v.begin(); it!=forward_v.end(); ++it)
-            for(auto it2 = backward_v.begin(); it2!=backward_v.end();++it2)
+
+        //this can be better
+        for(auto it = forward_v.begin(); it != forward_v.end() && intersect == ""; ++it)
+            for(auto it2 = backward_v.begin(); it2 != backward_v.end(); ++it2)
                 if(it->first==it2->first)
                 {
                     intersect = it->first;
                     break;
                 }
+
         if(!intersect.empty())
             break;
     }
-    if(intersect.empty())
-        cout<<"There is no word ladder from "<<begin<<" to "<<end<<endl;
+
+    vector<string> f_path;
+    vector<string> b_path;
+
+    //cout << intersect << endl; //this works :)
+
+    if(intersect == "")
+        cout << "There is no word ladder from " << begin << " to " << end << endl;
     else
     {
         front = intersect;
-        f_path.push_back(intersect);
-        front = forward_v[front];
-        while (front != begin) {
+        while (front != begin) 
+        {
             f_path.push_back(front);
             front = forward_v[front];
         }
+        
         f_path.push_back(begin);
-        backward = backward_v[intersect];
-        b_path.push_back(backward);
-        backward = backward_v[intersect];
-        while (backward != end) {
-            b_path.push_back(backward);
-            backward = forward_v[backward];
-        }
-        b_path.push_back(end);
-        for(auto it = f_path.begin(); it!=f_path.end(); ++it)
-            cheese+= *it+", ";
-        for(auto it = b_path.rbegin(); it!=b_path.rend(); ++it)
+
+        back = intersect;
+        back = backward_v[intersect];
+        while (back != end) 
         {
-            if(it+1==b_path.rend())
-                cheese+=*it;
-            else
-                cheese=*it+", ";
+            b_path.push_back(back);
+            back = backward_v[back];
         }
+        
+        b_path.push_back(end);
+
+        for(auto it = f_path.rbegin(); it != f_path.rend(); ++it)
+            cheese += *it + ", ";
+        //cout << cheese << endl;
+        for(auto it = b_path.begin(); it != b_path.end(); ++it)
+        {
+            if(it + 1 == b_path.end())
+                cheese += *it;
+            else
+                cheese += *it + ", ";
+        }
+        //cout << cheese << endl;
     }
-    visited=visitedCount;
+    visited = visitedCount;
     return cheese;
 }
 int main()
@@ -229,11 +261,13 @@ int main()
             database[length].insert(temp);
         }
     }
+
     auto difference = chrono::steady_clock::now()-start;
     auto duration = chrono::duration_cast<std::chrono::milliseconds>(difference);
-    cout<<duration.count()<<endl;
+    cout << duration.count() << endl;
     string begin;
     string end;
+
     do 
     {
         //continues prompting until a valid word is detected
@@ -253,14 +287,17 @@ int main()
     unordered_set<string> words = database[length];
     graph test(words,words.size());
     vector<string> tester;
+
     start = chrono::steady_clock::now();
     int numVisited;
-    string answer=biBFS(test,begin,end,numVisited);
-    difference = chrono::steady_clock::now()-start;
+
+    string answer = biBFS(test,begin,end,numVisited); //actual call
+    difference = chrono::steady_clock::now() - start;
     duration = chrono::duration_cast<std::chrono::milliseconds>(difference);
-    cout<<"Execution time of solving the word ladder: "<<duration.count()<< " Milliseconds"<<endl;
-    cout<<"Word Ladder: "<<answer<<endl;
-    cout<<"Number of nodes visited: "<<numVisited<<endl;
+
+    cout << "Execution time of solving the word ladder: " << duration.count() << " Milliseconds" << endl;
+    cout << "Word Ladder: " << answer << endl;
+    cout << "Number of nodes visited: " << numVisited << endl;
     return 0;
 }
 
